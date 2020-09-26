a956c7761af179ec71eec79eac5b8ef06f1962bb
usamahbass <basalamahusamah7@gmail.com>
2020-09-26 23:31:23 +0700

    adding branch deploy

---
+++ build/precache-manifest.ccf84d0fcdfa6fb5db5bbac81274e07a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c830c9f0e9dcbf07c930fe60668f553a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb0d604b5792d90857b7",
+    "url": "/static/css/main.e48ff3c3.chunk.css"
+  },
+  {
+    "revision": "14e5f9a0498d731bdec9",
+    "url": "/static/js/2.b0555411.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.b0555411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb0d604b5792d90857b7",
+    "url": "/static/js/main.ad0ab862.chunk.js"
+  },
+  {
+    "revision": "ff477d6f697ad9f58276",
+    "url": "/static/js/runtime-main.4fb12358.js"
+  }
+]);
